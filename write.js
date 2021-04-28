9ebf7db4bf7978073f96134f00131c362774bbd8
FatYellowDucky <82281912+FatYellowDucky@users.noreply.github.com>
2021-04-28 11:40:12 +0200

    Delete write.js

--- write.js
+++
@@ -1,8 +0,0 @@
-function write(address, newmem){
-	const fs = require('fs')
-	var x=fs.readFileSync(__dirname+'\\'+'mem.json').toString()
-	var mem=JSON.parse(x);
-	var y=''+address
-	mem.data[y] = newmem;
-	fs.writeFileSync(__dirname+'\\'+'mem.json', JSON.stringify(mem, null, '	'))
-} module.exports.write = write
