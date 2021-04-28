2e14cc9d1cc283387901ad5a32c3b1e168b4780b
FatYellowDucky <82281912+FatYellowDucky@users.noreply.github.com>
2021-04-28 11:39:16 +0200

    Delete read.js

--- read.js
+++
@@ -1,6 +0,0 @@
-function read(adress){
-	const fs = require('fs')
-	const x=fs.readFileSync(__dirname+'\\'+'mem.json').toString()
-	var y=JSON.parse(x)
-	return y.data[adress]
-} module.exports.read = read
