a5c4056ce82a588d1e0f9bfd95e3ce283a697535
FatYellowDucky <82281912+FatYellowDucky@users.noreply.github.com>
2021-04-28 11:40:04 +0200

    Delete deletememslot.js

--- deletememslot.js
+++
@@ -1,8 +0,0 @@
-function del(address){
-	const fs = require('fs');
-	var x=fs.readFileSync(__dirname+'\\'+'mem.json').toString();
-	var mem=JSON.parse(x);
-	var y=''+address;
-	mem.data[y] = undefined;
-	fs.writeFileSync(__dirname+'\\'+'mem.json', JSON.stringify(mem, null, '	'));
-} module.exports.del = del
